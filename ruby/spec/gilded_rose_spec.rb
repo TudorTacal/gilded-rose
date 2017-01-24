7d9fb4625969465118aca275c413b9916132916f
TudorTacal <tudortacal@gmail.com>
2017-01-24 20:50:24 +0000

    Tests that quality of non-special items can't go below 0

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -5,19 +5,32 @@ describe GildedRose do
 
   describe "#update_quality" do
 
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
-    end
-
     context 'for non-special items' do
+
       it "decreases the quality of the items with 1 per day" do
         items = [Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)]
         rose = GildedRose.new(items)
         expect{rose.update_quality()}.to change{items[0].quality}.from(20).to(19)
       end
+
+      it 'decreases the quality twice as fast if the sell by date has passed' do
+        items = [Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)]
+        rose = GildedRose.new(items)
+        10.times do
+          rose.update_quality()
+        end
+        expect{rose.update_quality()}.to change{items[0].quality}.from(10).to(8)
+      end
+
+      it "can't decrease the quality below 0" do
+        items = [Item.new(name="+5 Dexterity Vest", sell_in=1, quality=0)]
+        rose = GildedRose.new(items)
+        expect{rose.update_quality()}.to_not change{items[0].quality}
+      end
     end
   end
-
 end
+
+    # context 'for Aged Brie' do
+    #   it ''
+    # end
